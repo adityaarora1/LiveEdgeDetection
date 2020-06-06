a51fbdcf1142f3a7956d628864c0ea38a521e8e0
Hannes Achleitner <hannes.software@gmx.at>
2020-06-06 11:57:16 +0200

    smoke test

--- app/src/androidTest/java/info/hannes/liveedgedetection/demo/SmokeTest.kt
+++ app/src/androidTest/java/info/hannes/liveedgedetection/demo/SmokeTest.kt
@@ -2,7 +2,6 @@ package info.hannes.liveedgedetection.demo
 
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.adityaarora.liveedgedetection.MainActivity
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
